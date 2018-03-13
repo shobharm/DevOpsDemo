e0b98240dcd03e0d227b648db3e447fac4929933
shobha <m.shobha@tcs.com>
2018-03-13 15:35:40 +0530

    New changes for html

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,16 +1,17 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DemoController {
 
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String display() {
-		return "Succcessssss :P:P:P";
+		return "Index";
 	}
 	
 	
